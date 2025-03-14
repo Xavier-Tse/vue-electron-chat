23cea780f06c47139dc3da126415805f5c45f0e3
Xavier-Tse <xaviertse1024@163.com>
2025-03-14 20:03:22 +0800

    feat: conversation list component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <h1 class=" text-3xl font-bold bg-sky-300">💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <div class=" flex items-center justify-between h-screen">
+    <div class=" w-[300px] bg-gray-200 h-full border-r border-gray-300">
+      <ConversationList :items="items" />
+    </div>
+    <div class=" h-full flex-1">
+      content
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import ConversationList from './components/ConversationList.vue';
+import { ConversationProps } from './types'
+const items: ConversationProps[] = [
+  { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '标题', createdAt: '2021-01-01', updatedAt: '2021-01-01', providerId: 1 },
+]
 console.log('👋 This message is being logged by "renderer.ts", included via Vite')
 </script>
