7b09a2a52d08901a729a813952aea83b18c97303
Xavier-Tse <xaviertse1024@163.com>
2025-03-15 15:18:58 +0800

    feat: provider-select component

--- src/App.vue
+++ src/App.vue
@@ -21,20 +21,53 @@
         </button>
       </div>
     </div>
-    <div class=" h-full flex-1">
-      <ProviderSelect />
+    <div class=" h-full flex-1 flex items-center">
+      <div class="w-[80%] mx-auto h-full">
+        <div class="flex items-center h-[85%]">
+          <ProviderSelect :items="providers" v-model="selectedModel" />
+        </div>
+        <div class="flex items-center h-[15%]">
+          message input
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import ConversationList from './components/ConversationList.vue';
 import ProviderSelect from './components/ProviderSelect.vue';
-import { ConversationProps } from './types'
+import { ConversationProps, ProviderProps } from './types'
 import { Icon } from '@iconify/vue'
+
+const selectedModel = ref('')
 
 const items: ConversationProps[] = [
   { id: 1, selectedModel: 'GPT-3.5-Turbo', title: '标题', createdAt: '2021-01-01', updatedAt: '2021-01-01', providerId: 1 },
 ]
+
+const providers: ProviderProps[] = [
+  {     
+    id: 1,
+    name: '文心一言', 
+    desc: '文心一言 百度出品的大模型',
+    models: ['ERNIE-4.0-8K', 'ERNIE-3.5-8K', 'ERNIE-Speed-8K'],
+    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    createdAt: '2024-07-03',
+    updatedAt: '2024-07-03'
+  },
+  {     
+    id: 2,
+    name: '通义千问', 
+    desc: '通义千问',
+    // https://help.aliyun.com/zh/dashscope/developer-reference/api-details?spm=a2c4g.11186623.0.0.5bf41507xgULX5#b148acc634pfc
+    models: ['qwen-turbo', 'qwen-plus', 'qwen-max'],
+    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
+    createdAt: '2024-07-03',
+    updatedAt: '2024-07-03'
+  }
+]
+
 console.log('👋 This message is being logged by "renderer.ts", included via Vite')
 </script>
