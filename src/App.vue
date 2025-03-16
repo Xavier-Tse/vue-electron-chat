ca549806ef1979f7a0f5effa3cbfedb42e350086
Xavier-Tse <xaviertse1024@163.com>
2025-03-16 14:42:07 +0800

    feat: button component

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
       <div class=" h-[10%] grid grid-cols-2 gap-2 p-2">
         <RouterLink to="/">
           <button
-            class=" shadow-sm inline-flex items-center justify-center bg-green-700 text-white
-            hover:bg-green-700/90 border border-green-700 h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
+            class=" shadow-sm inline-flex items-center justify-center
+            bg-green-700 text-white hover:bg-green-700/90 border border-green-700 
+            h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
           >
             <Icon icon="radix-icons:chat-bubble" width="15" height="15" class=" mr-2" />
             新建聊天
@@ -16,8 +17,9 @@
         </RouterLink>
         <RouterLink to="/settings">
           <button
-            class=" shadow-sm inline-flex items-center justify-center bg-green-50 text-green-700
-          hover:bg-green-700 border border-green-700 hover:text-white h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
+            class=" shadow-sm inline-flex items-center justify-center 
+            bg-green-50 text-green-700 hover:bg-green-700 border border-green-700 hover:text-white 
+            h-[32px] py-[8px] px-[15px] text-sm rounded-[4px]"
           >
             <Icon icon="radix-icons:gear" width="15" height="15" class=" mr-2" />
             应用设置
@@ -26,6 +28,9 @@
       </div>
     </div>
     <div class=" h-full flex-1">
+      <Button color="purple" plain :loading="loading" icon-name="radix-icons:gear" @click="loading = true">
+        Hello there!
+      </Button>
       <RouterView />
     </div>
   </div>
@@ -35,6 +40,10 @@
 import ConversationList from './components/ConversationList.vue';
 import { Icon } from '@iconify/vue';
 import { conversations } from './testData';
+import Button from './components/Button.vue';
+import { ref } from 'vue';
+
+const loading = ref(false)
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite')
 </script>
